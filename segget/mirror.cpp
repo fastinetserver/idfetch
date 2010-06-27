3973e6d7e1c3760f843e8a2710a3fadc61389f21
Kostyantyn Ovechko <fastinetserver@gmail.com>
2010-06-27 20:44:41 +0300

    Failprove mirror.cpp and log errors to error.log

--- segget/mirror.cpp
+++ segget/mirror.cpp
@@ -27,51 +27,73 @@ class Tmirror{
 		uint get_active_num(){return active_num;};
 };
 
+map<string,Tmirror *> mirror_list;
+
 double Tmirror::mirror_on_the_wall(){
-	double criterion=honesty*1000000000*dld_time/dld_size;
-	honesty=honesty*100/(100+settings.benchmark_oblivion);
-	return criterion;
+	try{
+		double criterion=honesty*1000000000*dld_time/dld_size;
+		honesty=honesty*100/(100+settings.benchmark_oblivion);
+		return criterion;
+	}catch(...){
+		error_log("Error in mirror.cpp: mirror_on_the_wall()");
+		return 0;
+	}
 }
 void Tmirror::start(){
-	if (!dld_time)
-		dld_time=1;
-	active_num++;
-	honesty=1;
+	try{
+		if (!dld_time)
+			dld_time=1;
+		active_num++;
+		honesty=1;
+	}catch(...){
+		error_log("Error in mirror.cpp: start()");
+	}
 }
 
 void Tmirror::stop(ulong time, uint size){
-	dld_time+=time;
-	dld_size+=size;
-	honesty=1;
-	debug(toString(time)+"---"+toString(size));
-	active_num--;
+	try{
+		dld_time+=time;
+		dld_size+=size;
+		honesty=1;
+		debug(toString(time)+"---"+toString(size));
+		active_num--;
+	}catch(...){
+		error_log("Error in mirror.cpp: stop()");
+	}
 }
 
-map<string,Tmirror *> mirror_list;
-
 Tmirror* find_mirror(string mirror_url){
-	map<string,Tmirror*>::const_iterator mirror_iterator = mirror_list.find(mirror_url);
-	if (mirror_iterator==mirror_list.end()){
-		Tmirror * Pnew_mirror=new Tmirror;
-		debug("Cant find mirror:"+mirror_url+" - creating new record");
-		mirror_list[mirror_url]=Pnew_mirror;
-		return Pnew_mirror;
-	}
-	else{
-		debug("Found mirror:"+mirror_url);
-		debug("==================>>");
-		debug("       time:"+toString(mirror_iterator->second->dld_time));
-		debug("       size:"+toString(mirror_iterator->second->dld_size));
-		debug("    honesty:"+toString(mirror_iterator->second->honesty));
-		debug("  criterion:"+toString(mirror_iterator->second->mirror_on_the_wall()));
-		return mirror_iterator->second;
+	try{
+		map<string,Tmirror*>::const_iterator mirror_iterator = mirror_list.find(mirror_url);
+		if (mirror_iterator==mirror_list.end()){
+			Tmirror * Pnew_mirror=new Tmirror;
+			debug("Cant find mirror:"+mirror_url+" - creating new record");
+			mirror_list[mirror_url]=Pnew_mirror;
+			return Pnew_mirror;
+		}
+		else{
+			debug("Found mirror:"+mirror_url);
+			debug("==================>>");
+			debug("       time:"+toString(mirror_iterator->second->dld_time));
+			debug("       size:"+toString(mirror_iterator->second->dld_size));
+			debug("    honesty:"+toString(mirror_iterator->second->honesty));
+			debug("  criterion:"+toString(mirror_iterator->second->mirror_on_the_wall()));
+			return mirror_iterator->second;
+		}
+	}catch(...){
+		error_log("Error in mirror.cpp: find_mirror()");
+		return 0;
 	}
 }
 
 string strip_mirror_name(string path){
-	string mirror_name;
-	mirror_name=path.substr(0,path.find("/",(path.find("://",0)+3)));
-	return mirror_name;
+	try{
+		string mirror_name;
+		mirror_name=path.substr(0,path.find("/",(path.find("://",0)+3)));
+		return mirror_name;
+	}catch(...){
+		error_log("Error in mirror.cpp: strip_mirror_name()");
+		return "";
+	}
 }
-
 #endif
